1a03a43d1becade12f27fa0d1564f61a9435fa77
Jim Noble <jimnoble@xjjz.co.uk>
2012-03-03 07:11:45 +0000

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+AtFontFace::Application.config.secret_token = '74b0cad1071b32b8eda751ae85fd4e7636e005ed4cae4c29baa3c0ebbd9f677aa390242208213c5a52de6b0244abc3e91d0dec2f49cf8d705e82650da3989429'
